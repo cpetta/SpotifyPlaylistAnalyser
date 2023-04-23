10006af58a2a871d974f32cdd7e920515e2179b6
Chris Petta <cpetta@oaisd.org>
2023-04-23 17:46:51 -0400

    wip artist genre request spliting

--- album.py
+++ album.py
@@ -1,9 +1,10 @@
 
+import spotify_api
+
+from auth import Auth
+
 class Album:
-	def __init__(self, id:str) -> None:
+	def __init__(self, auth:Auth, id:str) -> None:
 		self.id:str = id
-		self.genres:list = self.get_genres()
-
-
-	def get_genres(self) -> list:
-		return []
+		# Album Genre's consistantly comes back blank.
+		# self.genres:list[str] = spotify_api.get_album_genre(auth, id)
--- spotify_api.py
+++ spotify_api.py
@@ -2,7 +2,6 @@ import requests
 
 from auth import Auth
 
-
 @staticmethod
 def get_related_artists(auth:Auth, artist):
 	API_endpoint = f'https://api.spotify.com/v1/artists/{artist.id}/related-artists'
@@ -24,3 +23,41 @@ def get_playlist(auth:Auth, id:str):
 	response = requests.get(url=API_endpoint, headers=auth.http_headers(), params=parameters) # type: ignore
 
 	return response.json()
+
+@staticmethod
+def get_several_artists_genres(auth:Auth, id_list:list[str]) -> list[str]:
+	genres = {}
+
+	API_endpoint = f'https://api.spotify.com/v1/artists'
+
+	i:int = 0
+	limit:int = 50
+	split_requests = []
+
+	for id in id_list:
+		if i >= limit:
+			i += 1
+			continue
+
+		split_requests[i].append(id)
+			
+	for request in split_requests:
+		ids:str = ','.join(request)
+		
+		parameters = {
+			"ids": ids
+		}
+
+		print(parameters)
+		# response = requests.get(url=API_endpoint, headers=auth.http_headers())
+	return []
+	# return response.json()['genres']
+
+@staticmethod
+def get_album_genre(auth:Auth, id:str) -> list[str]:
+	
+	API_endpoint = f'https://api.spotify.com/v1/albums/{id}'
+
+	response = requests.get(url=API_endpoint, headers=auth.http_headers())
+
+	return response.json()['genres']
