062269378468cc2322dec5a716f2b412fe23ce60
Chris Petta <cpetta@oaisd.org>
2023-04-23 13:25:50 -0400

    add artist sorting

--- main.py
+++ main.py
@@ -6,26 +6,23 @@ from settings import Settings
 
 settings = Settings()
 
+ARTIST_REC_LIMIT = 10
+GENRE_LIMIT = 5
 
 UI = CommandLineUI()
 auth = Auth(settings)
 playlist = Playlist(auth, UI.url)
 
-
-# Debug
-i:int = 0
-limit:int = 50
 print("Number of artists ", len(playlist.artists))
 recomended_artists_list:dict[str, Artist] = {};
 for artist in playlist.artists.values():
-	i += 1
-	if(i > limit):
-		break
 	recomended_artists_list = artist.get_related_artists(auth, recomended_artists_list)
 
 	for recomended_artist in recomended_artists_list.values():
 		id:str = recomended_artist.id
 		recomended_artists_list[id] = recomended_artist
 
-for recomended_artist in recomended_artists_list.values():
+ranked_recomended_artists = sorted(recomended_artists_list.values())
+
+for recomended_artist in ranked_recomended_artists:
 	print(recomended_artist)
